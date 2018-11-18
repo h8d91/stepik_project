2d11023f10bae8c4e17f0543764eeeb73b6e4393
h8d91 <sedoy01051991@gmail.com>
2018-11-18 23:34:30 +0300

    hello change

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,3 @@
 def application(env, start_response):
-start_response('200 OK', [('Content-Type', 'text/plain')])
-body = "\n".join(environ.get('QUERY_STRING').split("&"))
-return body
+	start_response('200 OK', [('Content-Type', 'text/plain')])
+	return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
