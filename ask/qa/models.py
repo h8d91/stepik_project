ec50e1aa3807a08f7f1a351d9e5f0c0575250963
h8d91 <sedoy01051991@gmail.com>
2018-11-29 20:54:58 +0300

    models.py and setting.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,39 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(default="", max_length=255)
+    text = models.TextField(default="")
+    added_at = models.DataTimeField(blank=True, auto_now_add=True)
+    rating = models.Integer(default=0)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='likes_set')
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return "/question/{}/".format(self.id)
+
+
+class Answer(models.Model):
+    text = models.TextField(default="")
+    added_at = models.DataTimeField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text
