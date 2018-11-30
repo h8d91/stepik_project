abcc8e3a56c04c2e3a0eb429e97fb3cf6fdb0f85
h8d91 <sedoy01051991@gmail.com>
2018-11-30 11:07:21 +0300

    mod setting.py in ask

--- ask/ask/settings — copy.py
+++ ask/ask/settings — copy.py
@@ -74,24 +74,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-if os.name == "nt" or True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'box_django',
-            'USER': 'box',
-            'PASSWORD': '1234',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        }
-    }
+}
 
 
 # Password validation
